80b61475b25c149f4db6451ec5983f8f715907b1
Dub Hokku <hokku@x081.spb.ru>
2021-02-08 09:12:00 +0300

    add nc_show_rpc_gen()

--- src/session_client.c
+++ src/session_client.c
@@ -2130,7 +2130,7 @@ nc_recv_reply(struct nc_session *session, struct nc_rpc *rpc, uint64_t msgid, in
 API NC_MSG_TYPE
 nc_show_rpc_gen( struct nc_session *session, struct nc_rpc* rp_request, char* data )
 {
-    
+    struct nc_rpc_act_generic* rpc_gen;
     switch( rp_request->type ) 
     {
         case NC_RPC_ACT_GENERIC:
