665bb0bdeb28a689867974fa1e8540b04516945a
Dub Hokku <hokku@x081.spb.ru>
2021-02-09 01:31:45 +0300

    add nc_show_rpc_gen()

--- src/session_client.c
+++ src/session_client.c
@@ -2159,7 +2159,7 @@ nc_show_rpc_gen( struct nc_session *session, struct nc_rpc* rp_request, char* te
     }
     
     lyd_print_mem( &text_request, lyd_request, LYD_XML, LYP_WITHSIBLINGS | LYP_NETCONF );
-    ERR( "text_request %s \n", text_request );
+    // ERR( "text_request %s \n", text_request );
     
     
     return NC_MSG_RPC;
